977ac20576e9afc98c5556f9ab66888f2881f900
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-03-05 03:30:44 -0600

    modified files corresponding to dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import "./global_styles/index.scss";
-
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Home from "./Home";
 import Compare from "./pages/Compare";
 import Playlists from "./pages/Playlists";
@@ -8,23 +7,47 @@ import Search from "./pages/Search";
 import Account from "./pages/Account";
 import Rankings from "./pages/Rankings";
 import { AuthProvider } from "./contexts/AuthContext";
-
-
+import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
-ReactDOM.render(
-  <Router>
-    <AuthProvider>
+const App = () => {
+  const { theme, setTheme } = useContext(ThemeContext); // Get the theme and setTheme function from context
+
+  useEffect(() => {
+    // Update the class of the html element based on the theme
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/compare" element={<Compare />} />
         <Route path="/playlists" element={<Playlists />} />
-        <Route path="/search" element={<Search />} /> 
+        <Route path="/search" element={<Search />} />
         <Route path="/account" element={<Account />} />
-        <Route path = "/rankings" element={<Rankings />} />
+        <Route path="/rankings" element={<Rankings />} />
       </Routes>
-    </AuthProvider>
-  </Router>,
+      <button
+        className="theme-container"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}>      
+        {theme === "light" ? "DARK" : "LIGHT"}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <ThemeProvider>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </ThemeProvider>,
   document.getElementById("root")
 );
