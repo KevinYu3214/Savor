66a098e7aee86b5e2c48f0d14d9163f88ea89c79
sxmcilrath <samuel.xavier.mac@gmail.com>
2024-03-19 12:59:17 -0500

    Song comp audio 'works'
    still need to find a way to switch between play and pause but audio plays when you click the song play button

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Link } from "react";
 import "../components/Song.scss";
 import playButton from "../assets/play-button-6.svg";
 
+const songClick = (songLink) => {
+  console.log("song clicked");
+  new Audio(songLink).play();
+};
 const Song = ({ result }) => {
   return (
     <div className="songContainer">
       <div className="songInfo">
-        <img src={result.album.images[1].url} className="songImage" />
-
-        <img src={playButton} className="playButton" />
+        <div className="songImageContainer">
+          <img src={result.album.images[1].url} className="songImage" />
+          <button
+            className="playButton"
+            onClick={() => songClick(result.preview_url)}
+          >
+            <img src={playButton} />
+          </button>
+        </div>
         <div className="songText">
           <div className="songTitle">{result.name}</div>
-          <div className="songAlbum">Song Album</div>
+          <div className="songAlbum">{result.album.name}</div>
         </div>
       </div>
 
       <div className="artistInfo">
         <img src={result.album.images[1].url} className="artistImage" />
-        <div className="artistName"> Artist Name</div>
+        <div className="artistName"> {result.artists[0].name}</div>
       </div>
     </div>
   );
