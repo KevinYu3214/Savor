cfab4dc45b31736400f3ad1c1d8515b93de49bf7
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-03-22 16:12:44 -0500

    Audio play/pause works

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,22 +1,42 @@
-import React, { Link } from "react";
+import React, { useState, useEffect } from "react";
 import "../components/Song.scss";
 import playButton from "../assets/play-button-6.svg";
 
-const songClick = (songLink) => {
-  console.log("song clicked");
-  new Audio(songLink).play();
-};
 const Song = ({ result }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState(null);
+
+  useEffect(() => {
+    const newAudio = new Audio(result.preview_url);
+    setAudio(newAudio);
+
+    return () => {
+      newAudio.pause();
+      setAudio(null); // Clean up the audio object
+    };
+  }, [result.preview_url]);
+
+  const togglePlayPause = () => {
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
   return (
     <div className="songContainer">
       <div className="songInfo">
         <div className="songImageContainer">
-          <img src={result.album.images[1].url} className="songImage" />
+          <img src={result.album.images[1].url} alt="Song" className="songImage" />
           <button
             className="playButton"
-            onClick={() => songClick(result.preview_url)}
+            onClick={togglePlayPause}
           >
-            <img src={playButton} />
+            <img src={playButton} alt="Play/Pause" />
           </button>
         </div>
         <div className="songText">
@@ -25,8 +45,8 @@ const Song = ({ result }) => {
         </div>
       </div>
       <div className="artistInfo">
-        <img src={result.album.images[1].url} className="artistImage" />
-        <div className="artistName"> {result.artists[0].name}</div>
+        <img src={result.album.images[1].url} alt="Artist" className="artistImage" />
+        <div className="artistName">{result.artists[0].name}</div>
       </div>
     </div>
   );
