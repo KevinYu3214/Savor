8aa034911f8d1c78602fa6ba4e3ee14eac600f80
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-03-22 23:39:41 -0500

    Clicking outside module closes it

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,41 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import SearchResult from "./SearchResult";
 import Song from "../components/Song";
 import styles from "./Search.module.scss";
 import ThemeContext from "../contexts/ThemeContext";
+import useOnClickOutside from "./useOnClickOutside"; // Adjust the path as needed
 
 const SearchResultsList = ({ results }) => {
   const [currResult, setCurrResult] = useState("");
   const [isShown, setIsShown] = useState(false);
-  const { theme } = useContext(ThemeContext); // Get the theme from context
+  const { theme } = useContext(ThemeContext);
+  const modalRef = useRef(); // Ref for the modal content
+
+  // Use the hook to close the modal when clicking outside
+  useOnClickOutside(modalRef, () => setIsShown(false));
 
   const handleClick = (result) => {
     setCurrResult(result);
-    setIsShown((isShown) => !isShown);
+    setIsShown(true);
   };
+
   return (
     <div>
       <div className={`${styles.search__results_list} ${styles[theme]}`}>
-        {results.map((result, id) => {
-          return (
-            <SearchResult
-              onClick={() => handleClick(result)}
-              result={result}
-              key={id}
-            />
-          );
-        })}
+        {results.map((result, id) => (
+          <SearchResult key={id} onClick={() => handleClick(result)} result={result} />
+        ))}
       </div>
       {isShown && (
-        <div className={`${styles.songContainer} ${isShown ? styles.overlay : ''}`}>
-          <Song result={currResult} />
-          <button onClick={() => setIsShown(false)} className={styles.closeButton}>
-          Close
-        </button>
+        <div className={styles.overlay}>
+          {/* Attach the ref to the modal content */}
+          <div ref={modalRef}>
+            <Song result={currResult} />
+          </div>
         </div>
-        
       )}
-
     </div>
   );
 };
