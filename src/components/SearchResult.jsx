c90e2f478e59d502e82666b23047ab661b6a3c3b
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-03-22 21:21:58 -0500

    Small UI update (fetching release date, etc).

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -6,12 +6,28 @@ const SearchResult = ({ result, onClick }) => {
     <div className={styles.search__results_list__result} onClick={onClick}>
       <img
         src={result.album.images[1].url}
+        alt="Album"
         className={styles.search__results_list__result__image}
       />
-      <div className={styles.search__results_list__result__title}>
-        {result.name}
+     
+      <div className={styles.infoColumn}>
+        <div className={styles.search__results_list__result__title}>
+          {result.name}
+        </div>
+        <div className={styles.search__results_list__result__info}>
+          <div className={styles.search__results_list__result__info__item}>
+            {result.artists[0].name}
+          </div>
+          <div className={styles.search__results_list__result__info__item}>
+            |
+          </div>
+          <div className={styles.search__results_list__result__info__item}>
+          {result.album.release_date}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
 export default SearchResult;
