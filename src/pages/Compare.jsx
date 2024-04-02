5774c6583f3e4184b59b87aa106a84a79d2508da
Silas Kyun <97808634+silaskyun@users.noreply.github.com>
2024-04-02 09:51:39 -0500

    Login necessary
    Making it necessary to login to savor account before connecting to Spotify. Functional but not final.

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Header from "../components/Header";
+import { useAuth } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 import CompareButton from "../components/CustomButtons/CompareButton";
-import { generateSpotifyAuthRequest } from "../spotify/Spotify"; // Assuming this is the adjusted function
+import { generateSpotifyAuthRequest, ensureValidToken } from "../spotify/Spotify"; // Assuming this is the adjusted function
 
 const Compare = () => {
     const [spotifyAuthRequest, setSpotifyAuthRequest] = useState('');
+    const { logout, currentUser } = useAuth();
+    const [missingUser, setMissingUser] = useState(currentUser?false:true);
+    const [token, setToken] = useState(null);
 
     useEffect(() => {
+        const checkToken = async () => {
+            const tmp = await ensureValidToken();
+            setToken(tmp);
+        }
         const fetchAuthUrl = async () => {
             const url = await generateSpotifyAuthRequest();
             setSpotifyAuthRequest(url);
@@ -14,8 +22,11 @@ const Compare = () => {
         fetchAuthUrl();
     }, []);
 
+
+
     return (
         <>
+            {missingUser && <Navigate to="/login" />}
             <mainCompareText>
                 Before we get into it... You need to <span>login</span>
             </mainCompareText>
