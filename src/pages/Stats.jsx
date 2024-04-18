e80fd29900aa50b566689647679f767105776000
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-04-17 22:07:06 -0500

    All categories work UI

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -21,6 +21,15 @@ import workout from "../assets/generatePlaylistImages/activity/workout.jpeg";
 import "./Stats.scss"; // Import the SCSS file
 import podium from "../assets/playlist icons/podium.png"; // Import
 
+import high_energy from "../assets/generatePlaylistImages/energy/high_energy.jpeg";
+import low_energy from "../assets/generatePlaylistImages/energy/low_energy.jpeg";
+import medium_energy from "../assets/generatePlaylistImages/energy/medium_energy.jpeg";
+
+import calm from "../assets/generatePlaylistImages/mood/calm.jpeg";
+import energetic from "../assets/generatePlaylistImages/mood/energetic.jpeg";
+import happy from "../assets/generatePlaylistImages/mood/happy.jpeg";
+import sad from "../assets/generatePlaylistImages/mood/sad.jpeg";
+
 const Stats = () => {
   const [profileName, setProfileName] = useState("");
   const [topTracks, setTopTracks] = useState([]);
@@ -36,6 +45,8 @@ const Stats = () => {
   const [imageColor, setImageColor] = useState(""); // State to store the dominant color extracted from the image
   const [secondaryColor, setSecondaryColor] = useState("");
   const [tertiaryColor, setTertiaryColor] = useState("");
+  const [category, setCategory] = useState("activity"); // Default category is "activity"
+  const [categoryIndex, setCategoryIndex] = useState(0); // State to track the category index
 
   const handlePlaylistClick = (contents) => {
     setToDisplay(contents);
@@ -166,13 +177,114 @@ const Stats = () => {
     fetchData();
   }, []);
 
-  const handleTagClick = async (image, text) => {
-    if (selectedImage !== image) { // Check if the selected image has changed
-      setCurrentImage(image);
+
+  const actionTexts = ["doing", "energy", "mood"];
+
+
+  const activityEmojis = ["😴", "🧹", "🍳", "🚗", "🎉", "📚", "💪"];
+  const activityTexts = ["sleep", "clean", "cook", "drive", "party", "study", "gym"];
+  const activityImages = [
+    sleeping, // URL for sleeping image
+    cleaning, // URL for cleaning image
+    cooking, // URL for cooking image
+    driving, // URL for driving image
+    party, // URL for party image
+    studying, // URL for studying image
+    workout // URL for workout image
+  ];
+  
+
+  const energyEmojis = ["⚡️", "🔋", "🌀"];
+  const energyTexts = ["high", "medium energy", "low"];
+  const energyImages = [
+    high_energy, // URL for high energy image
+    medium_energy, // URL for medium energy image
+    low_energy // URL for low energy image
+  ];
+
+  const moodEmojis = ["😊", "😄", "😢", "😎"];
+  const moodTexts = ["happy", "energetic", "sad", "calm"];  
+  const moodImages = [
+    happy, // URL for happy mood image
+    energetic, // URL for energetic mood image
+    sad, // URL for sad mood image
+    calm // URL for calm mood image
+  ];
+  
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+    switch (newCategory) {
+      case "activity":
+        setCurrentText("sleep");
+        setSelectedImage(sleeping);
+        setCategoryIndex(0);
+        break;
+      case "energy":
+        setCurrentText("medium");
+        setSelectedImage(medium_energy);
+        setCategoryIndex(1);
+        break;
+      case "mood":
+        setCurrentText("calm");
+        setSelectedImage(calm);
+        setCategoryIndex(2);
+        break;
+      default:
+        setCurrentText("doing");
+        setSelectedImage(sleeping);
+        setCategoryIndex(0);
+        break;
+    }
+  };
+
+  const renderTagButtons = () => {
+    switch (category) {
+      case "activity":
+        return activityEmojis.map((emoji, index) => (
+          <button key={index} onClick={() => handleTagClick(activityImages[index], emoji, activityTexts[index])}>
+            {emoji}
+          </button>
+        ));
+      case "energy":
+        return energyEmojis.map((emoji, index) => (
+          <button key={index} onClick={() => handleTagClick(energyImages[index], emoji, energyTexts[index])}>
+            {emoji}
+          </button>
+        ));
+      case "mood":
+        return moodEmojis.map((emoji, index) => (
+          <button key={index} onClick={() => handleTagClick(moodImages[index], emoji, moodTexts[index])}>
+            {emoji}
+          </button>
+        ));
+      default:
+        return null;
+    }
+  };
+  
+  
+  const handleTagClick = async (image, emoji, text) => {
+    let imageUrl = ""; // Initialize imageUrl variable to store the selected image URL
+    switch (category) {
+      case "activity":
+        imageUrl = activityImages[activityEmojis.indexOf(emoji)];
+        break;
+      case "energy":
+        imageUrl = energyImages[energyEmojis.indexOf(emoji)];
+        break;
+      case "mood":
+        imageUrl = moodImages[moodEmojis.indexOf(emoji)];
+        break;
+      default:
+        return;
+    }
+    
+    if (selectedImage !== imageUrl) { // Check if the selected image has changed
+      setCurrentImage(imageUrl);
       setCurrentText(text);
-      setMaskTextBackground(image); // Set the background image for maskText
-      setSelectedImage(image); // Set the selected image
-      extractDominantColor(image); // Extract dominant color when a tag is clicked
+      setMaskTextBackground(imageUrl); // Set the background image for maskText
+      setSelectedImage(imageUrl); // Set the selected image URL
+      extractDominantColor(imageUrl); // Extract dominant color when a tag is clicked
     }
   };
   
@@ -181,10 +293,8 @@ const Stats = () => {
   return (
     <div>
       <h1>Spotify Profile</h1>
-      {isLoading && <p>Loading...</p>}{" "}
-      {/* Display loading message if data is being fetched */}
-      {!isLoading && error && <p>Error: {error}</p>}{" "}
-      {/* Display error message if there's an error */}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && error && <p>Error: {error}</p>}
       {!isLoading && profileName && <p>Welcome, {profileName}</p>}
       {!isLoading && topTracks.length > 0 && (
         <div>
@@ -223,7 +333,6 @@ const Stats = () => {
             />
             <h3 className="playlistTitle">Suggested Tracks</h3>
           </div>
-
           {playlistShown && (
             <div className="overlayStats" onClick={handleOverlayClick}>
               <div onClick={(e) => e.stopPropagation()}>
@@ -231,54 +340,30 @@ const Stats = () => {
               </div>
             </div>
           )}
-          {/* <ul>
-                        {suggestedPlaylist.map(track => (
-                             <Song result={track} />
-                        ))}
-                    </ul> */}
         </div>
       )}
       <div className="orchid-container" 
         style={{ "--custom-color": imageColor, "--secondary-color": secondaryColor,
         "--tertiary-color": tertiaryColor }}>
-        {/* Add the orchid container */}
         <div className="custom-div">
           <p>
-            <span></span>now:
+            <span></span>{actionTexts[categoryIndex]}:
             <br />
             <i>{currentText}</i>
           </p>
           <div className="custom-image-container">
-          <ColorExtractor getColors={extractDominantColor}>
+            <ColorExtractor getColors={extractDominantColor}>
               <img src={selectedImage} alt="Selected" />
             </ColorExtractor>
-
           </div>
-          <image></image>
-                  </div>
+        </div>
         <div className="tags">
-          <button onClick={() => handleTagClick(sleeping, "sleep")}>
-            😴
-          </button>
-          <button onClick={() => handleTagClick(cleaning, "clean")}>
-            🧹
-          </button>
-          <button onClick={() => handleTagClick(cooking, "cook")}>
-            🍳
-          </button>
-          <button onClick={() => handleTagClick(driving, "drive")}>
-            🚗
-          </button>
-          <button onClick={() => handleTagClick(party, "party")}>
-            🎉
-          </button>
-          <button onClick={() => handleTagClick(studying, "study")}>
-            📚
-          </button>
-          <button onClick={() => handleTagClick(workout, "gym")}>
-            💪
-          </button>
-
+        {renderTagButtons()}
+        </div>
+        <div className="category-buttons">
+          <button onClick={() => handleCategoryChange("activity")}>Activity</button>
+          <button onClick={() => handleCategoryChange("energy")}>Energy</button>
+          <button onClick={() => handleCategoryChange("mood")}>Mood</button>
         </div>
       </div>
     </div>
