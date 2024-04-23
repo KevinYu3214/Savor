a9c5a298223d6b5cd8ff0e37880263c0f0282634
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-04-23 08:54:40 -0500

    made stats empty

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,37 +1,12 @@
-import { useContext, useState } from "react";
-import MusicList from "../components/MusicList";
-import { ThemeContext } from "../contexts/ThemeContext";
-import PlaylistComponent from "../components/PlaylistComponent";
-import workout from "../assets/playlist icons/workout.jpg";
-import "../global_styles/Stats.scss";
+import React, { useContext, useState } from 'react'
 
 const Stats = () => {
-  const [isShown, setIsShown] = useState(false);
+ 
 
-  const handleClick = () => {
-    console.log("clicked");
-    setIsShown(!isShown);
-  };
-  const { theme } = useContext(ThemeContext);
   return (
-    <body className={theme === "light" ? "light" : "dark"}>
-      <div className="browsePL">
-        <img className="workoutPL" src={workout} onClick={handleClick} />
-      </div>
-      {isShown && (
-        <div className="overlayPL" onClick={handleClick}>
-          <div onClick={(e) => e.stopPropagation()}>
-            {/*<PlaylistComponent></PlaylistComponent>*/}
-          </div>
-        </div>
-      )}
-      <h1>
-        Top Music<span>Today</span>
-      </h1>
-      {/*<PlaylistComponent />*/}
-    </body>
-  );
-};
+    <>
+    </>
+  )
+}
 
-export default Stats;
-
+export default Stats
