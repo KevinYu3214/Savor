1dd4e37a6757266094777f1e13b68d1365005e4d
jacobbwalshh <122336663+jacobbwalshh@users.noreply.github.com>
2024-04-25 22:25:57 -0500

    Added some styling to the preferences component

--- src/components/SavedPreference.jsx
+++ src/components/SavedPreference.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import "./SavedPreference.module.scss";
 import { auth, db } from "../firebase/firebase";
-import SearchBar from "./Search/SearchBar"
+import SearchBar from "./Search/SearchBar";
 import PreferenceResultsList from "./Search/PreferenceResultsList";
 import {
   getDocs,
@@ -26,14 +26,14 @@ const SavedPreference = () => {
       preferencesCollectionList,
       where("userId", "==", auth.currentUser.uid)
     );
-    const preferencesQuerySnapshot = await getDocs(preferencesQuery);   
+    const preferencesQuerySnapshot = await getDocs(preferencesQuery);
     const songIDList = preferencesQuerySnapshot.docs.map((doc) => doc.data().songId);
 
     // Query to find songs
     if (songIDList.length !== 0) {
       const songQuery = query(
-          songCollectionList,
-          where("songId", "in", songIDList)
+        songCollectionList,
+        where("songId", "in", songIDList)
       );
       const songQuerySnapshot = await getDocs(songQuery);
       const songs = songQuerySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
@@ -52,21 +52,21 @@ const SavedPreference = () => {
 
   const deletePreference = useCallback(async (songId) => {
     if (!songId) return;
-    
+
     const preferencesQuery = query(
       preferencesCollectionList,
       where("songId", "==", songId),
       where("userId", "==", auth.currentUser.uid)
     );
     const querySnapshot = await getDocs(preferencesQuery);
-  
+
     if (!querySnapshot.empty) {
       const preferencesDoc = querySnapshot.docs[0];
       await deleteDoc(doc(db, "Preferences", preferencesDoc.id));
       console.log("Song deleted successfully");
-      
+
       // Remove the deleted song from the songList state
-      setSongList(prevSongList => prevSongList.filter(song => song.songId !== songId));
+      setSongList((prevSongList) => prevSongList.filter((song) => song.songId !== songId));
     } else {
       console.error("Song not found");
     }
@@ -74,22 +74,30 @@ const SavedPreference = () => {
 
   return (
     <>
-      <div className="basic">Add songs that will be used to generate playlists!</div>
-      <div>5 choices maximum</div>
-      <SearchBar setResults={setResults} /> 
-      {songCount <= 4 && <PreferenceResultsList results={results} setParam={setRecentSong}/>}
+      <div className="preference_saved-preference-container">
+        <div className="preference_header">Add songs that will be used to generate playlists!</div>
+        <div className="preference_instruction">5 choices maximum</div>
+        <SearchBar setResults={setResults} />
+        {songCount <= 4 && <PreferenceResultsList results={results} setParam={setRecentSong} />}
 
-      <div>
-        <div>Your Selections</div>
-        {songList.length > 0 && songList.map((song) =>
-          <div key={song.songId}>
-            <img src={song.image} alt="Song" />
-            <div>{song.title}</div>
-            <div>{song.artist}</div>
-            <div>{song.album}</div>
-            <div onClick={() => deletePreference(song.songId)}>delete</div>
-          </div>
-        )}  
+        <div className="preference_selections">
+          <div className="preference_selections-header">Your Selections:</div>
+          {songList.length > 0 &&
+            songList.map((song) => (
+              <div key={song.songId} className="preference_song-item">
+                <img src={song.image} alt="Song" />
+                <div className="preference_song-details">
+                  <div className="preference_title">{song.title}</div>
+                  <div className="preference_artist">{song.artist}</div>
+                  <div className="preference_album">{song.album}</div>
+                </div>
+                <div className="preference_delete" onClick={() => deletePreference(song.songId)}>
+                  Delete
+                </div>
+              </div>
+            ))}
+            {songList.length == 0 && <div className="preference_instruction">No selections made!</div>}
+        </div>
       </div>
     </>
   );
